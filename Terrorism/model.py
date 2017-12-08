4589d47052ac97e20cc7bf8ab37c6e5d173ce879
Stefano <steno87@hotmail.com>
2017-12-08 15:49:02 +0100

    completed

--- Terrorism/model.py
+++ Terrorism/model.py
@@ -1,12 +1,73 @@
+from keras import Sequential
+from keras.layers import LSTM, Dense
+from matplotlib import pyplot
 from pandas import read_csv
+from sklearn.preprocessing import MinMaxScaler
+from utils.data_preprocessing import date_parsing, series_to_supervised
 
 dataset = read_csv(
     filepath_or_buffer='dataset/globalterrorismdb_0617dist.csv',
     sep=',',
-    header=0,
+    index_col=0,
     encoding='ISO-8859-1',
-    usecols=['iyear','imonth','iday','country', 'country_txt']
+    usecols=['iyear', 'imonth', 'iday', 'country', 'country_txt', 'latitude', 'longitude', 'nkill'],
+    parse_dates=[['iyear', 'imonth', 'iday']],
+    date_parser=date_parsing
 )
 
+dataset.index.name = 'date'
+dataset['nkill'].fillna(0, inplace=True)
 
-print(dataset.tail())
+encoded_country = dict(zip(dataset['country'], dataset['country_txt']))
+decoded_country = dict(zip(dataset['country_txt'], dataset['country']))
+
+dataset.drop('country_txt', inplace=True, axis=1)
+dataset.dropna(inplace=True)
+
+values = dataset.values
+values = values.astype('float32')
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+
+reframed = series_to_supervised(scaled)
+
+train_split = 0.75
+train_split = round(train_split * reframed.shape[0])
+
+values = reframed.values
+
+train = values[:train_split, :]
+test = values[train_split:, :]
+
+train_X, train_y = train[:, :4], train[:, 4:]
+test_X, test_y = test[:, :4], test[:, 4:]
+
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+
+model = Sequential()
+
+model.add(LSTM(
+    units=50,
+    input_shape=(train_X.shape[1], train_X.shape[2]),
+))
+
+model.add(Dense(units=4))
+model.compile(loss='mae', optimizer='adam')
+
+history = model.fit(x=train_X,
+                    y=train_y,
+                    epochs=50,
+                    batch_size=72,
+                    validation_data=(test_X, test_y),
+                    verbose=2,
+                    shuffle=False
+                    )
+
+# plot history
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
+
--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -1,6 +1,8 @@
 from numpy import loadtxt
 from os import path, makedirs
 from urllib import request
+from datetime import datetime
+from pandas import DataFrame, concat
 
 
 def load_dataset(filename):
@@ -33,5 +35,50 @@ def fetch_dataset(url, filename):
         print("Dataset saved in " + filename)
 
 
+def date_parsing(x, y, z):
+    """
+
+    :param z:
+    :param y:
+    :param x: array
+    :return:
+    """
+    if z == '0':
+        z = '1'
+    if y == '0':
+        y = '1'
+    x = ' '.join([x, y, z])
+    return datetime.strptime(x, '%Y %m %d')
 
 
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    """
+    Frame a time series as a supervised learning dataset.
+
+    :param data: Sequence of observations as a list or NumPy array.
+    :param n_in: Number of lag observations as input (X).
+    :param n_out: Number of observations as output (y).
+    :param dropnan: Boolean whether or not to drop rows with NaN values.
+    :return: Pandas DataFrame of series framed for supervised learning.
+    """
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
